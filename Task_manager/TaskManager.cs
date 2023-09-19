1278162ac713c8f79fc11d5bb3a1e669cbabfecb
Ефимов Максим <efimov_m@itstep.academy>
2023-09-19 21:01:44 +0300

    firsrt

--- Task_manager/TaskManager.cs
+++ Task_manager/TaskManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Task_manager
@@ -17,24 +18,44 @@ namespace Task_manager
         {
             Console.WriteLine("Привет, что хочешь создать: 1 - встреча, 2 - событие");
             string answer = Console.ReadLine();
-            if(Int32.TryParse(answer, out int result)
+            if(Int32.TryParse(answer, out int result))
             {
                 switch(result)
                 {
                     case 1:
                         Console.WriteLine("Введити название");
-                        string name = Console.ReadLine();
+                        string name_meeting = Console.ReadLine();
                         Console.WriteLine("Введите описание");
-                        string description = Console.ReadLine();
+                        string description_meeting = Console.ReadLine();
                         Console.WriteLine("Введите дату и время оповещения");
-
+                        string evendDate_meeting = Console.ReadLine();
+                        Console.WriteLine("Введите дату и время начала");
+                        string str_beginDate_meeting = Console.ReadLine();
+                        Console.WriteLine("Введите дату и время окончания");
+                        string str_endDate_meeting = Console.ReadLine();
+                        Console.WriteLine("Введите список участников");
+                        string members = Console.ReadLine();
+                        DateTime.TryParse(str_beginDate_meeting, out DateTime beginDate);
+                        DateTime.TryParse(str_endDate_meeting, out DateTime endDate);
+                        tasks.Add(new Meeting(name_meeting, description_meeting, beginDate, endDate, members.Split(new char[] { ' ' }).ToList()));
                         break;
+                    case 2:
+                        Console.WriteLine("Введити название");
+                        string name_event = Console.ReadLine();
+                        Console.WriteLine("Введите описание");
+                        string description_event = Console.ReadLine();
+                        Console.WriteLine("Введите дату и время оповещения");
+                        string evendDate_event = Console.ReadLine();
+                        DateTime.TryParse(evendDate_event, out DateTime event_date);
+                        tasks.Add(new Event(name_event, description_event,event_date));
+                        break;
+
 
                 }
             }
 
         }
-        public void DeleteTask();
+      //  public void DeleteTask();
         public int Length()
         {
             return tasks.Count;
